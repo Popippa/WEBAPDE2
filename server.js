e00dfa5c43b522bdd4f3f63dd01ed3f2cb45c079
chris <alcalachristianfrederick@gmail.com>
2019-08-23 16:45:25 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,11 @@ app.use(session({
 }))
 
 mongoose.Promise = global.Promise
-mongoose.connect("mongodb+srv://christian:tokisaki@cluster0-qjj0u.mongodb.net/test?retryWrites=true&w=majority", {
+//mongoose.connect("mongodb+srv://christian:tokisaki@cluster0-qjj0u.mongodb.net/test?retryWrites=true&w=majority", {
+    mongoose.connect("mongodb+srv://fan34:can34@type-racer-mli3n.mongodb.net/test?retryWrites=true&w=majority", {
     useNewUrlParser:true,
     useFindAndModify: false,
-  
+    //dbName:"mark"
 })
 
 //Get the default connection
